b8e85f110168bc102bc52bdb65f2226e96a23724
SoftechMansoor <softech1900@gmail.com>
2020-05-09 15:52:19 +0500

    Summary

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
- <footer class="foo">
-            <div class="row">
-                <div class="col-lg-12">
-                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:17px;"><i class="material-icons" style="color: brown;">COPYRIGHT</i> 2020 <b >Online Learning System <a href="https://softech.technology">Softech</a></b></p>
-                </div>
-            </div>
-   </footer>
